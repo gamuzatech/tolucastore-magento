c2f7453e1605cd956e87e900bb86d4cbec8c2931
Eneias Ramos de Melo <eneias@gamuza.com.br>
2021-10-07 12:34:13 -0300

    Atualizado modulo Mage_ImportExport: ajustado tradução

---
+++ app/code/local/Gamuza/Basic/Model/ImportExport/Source/Import/Entity.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @package     Gamuza_Basic
+ * @copyright   Copyright (c) 2021 Gamuza Technologies (http://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+/**
+ * See the AUTHORS file for a list of people on the Gamuza Team.
+ * See the ChangeLog files for a list of changes.
+ * These files are distributed with gamuza_basic-magento at http://github.com/gamuzatech/.
+ */
+
+/**
+ * Source import entity model
+ */
+class Gamuza_Basic_Model_ImportExport_Source_Import_Entity extends Mage_ImportExport_Model_Source_Import_Entity
+{
+    /**
+     * Prepare and return array of import entities ids and their names
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $options = array();
+        $entities = Mage_ImportExport_Model_Import::CONFIG_KEY_ENTITIES;
+        $comboOptions = Gamuza_Basic_Model_ImportExport_Config::getModelsComboOptions($entities);
+
+        foreach ($comboOptions as $option)
+        {
+           $options[] = $option;
+        }
+
+        return $options;
+    }
+}
+
