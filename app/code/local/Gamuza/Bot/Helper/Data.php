fa3212657aabc924fa0b1409661c5b52dc128abc
Eneias Ramos de Melo <eneias@gamuza.com.br>
2021-11-10 20:50:00 -0300

    Adicionado modulo Gamuza_Bot

---
+++ app/code/local/Gamuza/Bot/Helper/Data.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package     Gamuza_Bot
+ * @copyright   Copyright (c) 2020 Gamuza Technologies (http://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+class Gamuza_Bot_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    const ORDER_ATTRIBUTE_IS_BOT = 'is_bot';
+
+    const STATUS_BOT      = 'bot';
+    const STATUS_CATEGORY = 'category';
+    const STATUS_PRODUCT  = 'product';
+    const STATUS_OPTION   = 'option';
+    const STATUS_VALUE    = 'value';
+    const STATUS_BUNDLE   = 'bundle';
+    const STATUS_SELECTION = 'selection';
+    const STATUS_COMMENT  = 'comment';
+    const STATUS_CART     = 'cart';
+    const STATUS_ADDRESS  = 'address';
+    const STATUS_SHIPPING = 'shipping';
+    const STATUS_PAYMENT  = 'payment';
+    const STATUS_PAYMENT_CASH = 'payment_cash';
+    const STATUS_PAYMENT_MACHINE = 'payment_machine';
+    const STATUS_CHECKOUT = 'checkout';
+    const STATUS_ORDER    = 'order';
+}
+
---
+++ app/code/local/Gamuza/Bot/sql/bot_setup/mysql4-install-0.0.1.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * @package     Gamuza_Bot
+ * @copyright   Copyright (c) 2020 Gamuza Technologies (http://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+$installer = $this;
+$installer->startSetup ();
+
+function addBotQueueTable ($installer, $model, $comment)
+{
+    $table = $installer->getTable ($model);
+
+    $sqlBlock = <<< SQLBLOCK
+CREATE TABLE IF NOT EXISTS {$table}
+(
+    entity_id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(entity_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 COMMENT='{$comment}';
+SQLBLOCK;
+
+    $installer->run ($sqlBlock);
+
+    $installer->getConnection ()
+        ->addColumn ($table, 'store_id', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_INTEGER,
+            'length'   => 11,
+            'unsigned' => true,
+            'nullable' => false,
+            'comment'  => 'Store ID',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'quote_id', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_INTEGER,
+            'length'   => 11,
+            'unsigned' => true,
+            'nullable' => false,
+            'comment'  => 'Quote ID',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'order_id', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_INTEGER,
+            'length'   => 11,
+            'unsigned' => true,
+            'nullable' => false,
+            'comment'  => 'Order ID',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'category_id', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_INTEGER,
+            'length'   => 11,
+            'unsigned' => true,
+            'nullable' => false,
+            'comment'  => 'Category ID',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'product_id', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_INTEGER,
+            'length'   => 11,
+            'unsigned' => true,
+            'nullable' => false,
+            'comment'  => 'Product ID',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'bundle_id', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_INTEGER,
+            'length'   => 11,
+            'unsigned' => true,
+            'nullable' => false,
+            'comment'  => 'Bundle ID',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'selections', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'length'   => 255,
+            'nullable' => false,
+            'comment'  => 'Selections',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'option_id', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_INTEGER,
+            'length'   => 11,
+            'unsigned' => true,
+            'nullable' => false,
+            'comment'  => 'Option ID',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'options', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'length'   => 255,
+            'nullable' => false,
+            'comment'  => 'Options',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'comment', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'length'   => 255,
+            'nullable' => false,
+            'comment'  => 'Comment',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'remote_ip', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'length'   => 255,
+            'nullable' => false,
+            'comment'  => 'Remote IP',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'number', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'length'   => 255,
+            'nullable' => false,
+            'comment'  => 'Number',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'firstname', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'length'   => 255,
+            'nullable' => false,
+            'comment'  => 'Firstname',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'lastname', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'length'   => 255,
+            'nullable' => false,
+            'comment'  => 'Lastname',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'email', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'length'   => 255,
+            'nullable' => false,
+            'comment'  => 'Email',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'status', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'length'   => 255,
+            'nullable' => false,
+            'comment'  => 'Status',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'is_muted', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_INTEGER,
+            'length'   => 11,
+            'unsigned' => true,
+            'nullable' => false,
+            'comment'  => 'Is Muted',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'is_notified', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_INTEGER,
+            'length'   => 11,
+            'unsigned' => true,
+            'nullable' => false,
+            'comment'  => 'Is Notified',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'created_at', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_DATETIME,
+            'nullable' => false,
+            'comment'  => 'Created At',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'updated_at', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_DATETIME,
+            'nullable' => true,
+            'comment'  => 'Updated At',
+        ));
+}
+
+addBotQueueTable ($installer, 'gamuza_bot_queue', 'Gamuza Bot Queue');
+
+$installer->endSetup ();
+
