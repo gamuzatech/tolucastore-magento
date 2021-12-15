80ea43d51646ece6e59727bff27c94e1b75a608b
Eneias Ramos de Melo <eneias@gamuza.com.br>
2021-12-14 22:56:36 -0300

    Atualizado modulo Gamuza_Basic: ajustado rotina para geração de IncrementId baseado no tipo de terminal acessado (BOT via controller/action)

--- app/code/local/Gamuza/Basic/Model/Eav/Entity/Type.php
+++ app/code/local/Gamuza/Basic/Model/Eav/Entity/Type.php
@@ -47,8 +47,9 @@ class Gamuza_Basic_Model_Eav_Entity_Type extends Mage_Eav_Model_Entity_Type
         $isApp = $currentOrder && $currentOrder->getData (Gamuza_Basic_Helper_Data::ORDER_ATTRIBUTE_IS_APP);
 
         $isBot = Mage::helper ('core')->isModuleEnabled ('Gamuza_Bot')
-            && !strcmp (Mage::app ()->getRequest ()->getControllerModule (), 'Gamuza_JsonApi')
-            && strpos (Mage::app ()->getRequest ()->getRawBody (), self::API_METHOD_BOT_QUEUE_MESSAGE) !== false
+            && (!strcmp (Mage::app ()->getRequest ()->getControllerModule (), 'Gamuza_Bot')
+            xor (!strcmp (Mage::app ()->getRequest ()->getControllerModule (), 'Gamuza_JsonApi')
+            && strpos (Mage::app ()->getRequest ()->getRawBody (), self::API_METHOD_BOT_QUEUE_MESSAGE) !== false))
         ;
 
         if ($isMobile || $isApp)
