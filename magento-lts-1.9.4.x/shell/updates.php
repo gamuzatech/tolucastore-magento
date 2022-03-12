2b75adfa002d35cb8e1eced073f5e8513b29b218
Eneias Ramos de Melo <eneias@gamuza.com.br>
2022-03-12 13:56:50 -0300

    Adicionado shell script updates.php

---
+++ magento-lts-1.9.4.x/shell/updates.php
@@ -0,0 +1,46 @@
+<?php
+
+// Change current directory to the directory of current script
+chdir(dirname(__FILE__));
+
+require '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'bootstrap.php';
+require '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'Mage.php';
+
+if (!Mage::isInstalled())
+{
+    echo "Application is not installed yet, please complete install wizard first.";
+
+    exit;
+}
+
+// Only for urls
+// Don't remove this
+$_SERVER['SCRIPT_NAME'] = str_replace(basename(__FILE__), 'index.php', $_SERVER['SCRIPT_NAME']);
+$_SERVER['SCRIPT_FILENAME'] = str_replace(basename(__FILE__), 'index.php', $_SERVER['SCRIPT_FILENAME']);
+
+try
+{
+    Mage::app('admin')->setUseSessionInUrl(false);
+}
+catch (Exception $e)
+{
+    echo $e->getMessage() . PHP_EOL;
+
+    exit;
+}
+
+umask(0);
+
+try
+{
+    Mage_Core_Model_Resource_Setup::applyAllUpdates ();
+
+    Mage_Core_Model_Resource_Setup::applyAllDataUpdates ();
+}
+catch (Exception $e)
+{
+    echo $e->getMessage() . PHP_EOL;
+
+    exit(1);
+}
+
