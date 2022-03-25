36ec3bc6ca09b637b0efdddd3c67105ee5bef65f
Eneias Ramos de Melo <eneias@gamuza.com.br>
2022-03-25 13:28:21 -0300

    Atualizado modulo Gamuza_MercadoLivre: ajustado observer para root_category_id

--- magento-lts-1.9.4.x/app/code/local/Gamuza/MercadoLivre/Model/Observer.php
+++ magento-lts-1.9.4.x/app/code/local/Gamuza/MercadoLivre/Model/Observer.php
@@ -11,9 +11,17 @@ class Gamuza_MercadoLivre_Model_Observer
     {
         $collection = $observer->getEvent ()->getCategoryCollection ();
 
-        if (!$collection->hasFlag ('mercadolivre'))
+        $treeCategoryId = Mage_Catalog_Model_Category::TREE_ROOT_ID;
+        $rootCategoryId = Mage::getStoreConfig (Gamuza_MercadoLivre_Helper_Data::XML_PATH_MERCADOLIVRE_SETTINGS_ROOT_CATEGORY_ID);
+
+        if (!$collection->hasFlag ('mercadolivre') && intval ($rootCategoryId) > 0)
         {
-            $collection->addAttributeToFilter ('name', array ('neq' => 'MercadoLivre'));
+            $collection->getSelect ()
+                ->where (sprintf ("NOT (path = '%s/%s' OR path LIKE '%s/%s/%%')",
+                    $treeCategoryId, $rootCategoryId,
+                    $treeCategoryId, $rootCategoryId,
+                ))
+            ;
 
             $observer->getEvent ()->setCategoryCollection ($collection);
         }
