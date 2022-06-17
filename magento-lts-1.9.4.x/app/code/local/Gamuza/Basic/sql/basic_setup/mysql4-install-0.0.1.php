53a266b221859996c5d1b252a712acd2e98105bb
Eneias Ramos de Melo <eneias@gamuza.com.br>
2022-06-16 22:49:04 -0300

    Atualizado modulo Gamuza_Basic: ajustado opções padrão para web em config.xml

--- magento-lts-1.9.4.x/app/code/local/Gamuza/Basic/sql/basic_setup/mysql4-install-0.0.1.php
+++ magento-lts-1.9.4.x/app/code/local/Gamuza/Basic/sql/basic_setup/mysql4-install-0.0.1.php
@@ -21,6 +21,10 @@ $coreConfig->saveConfig ('general/country/allow', 'BR');
  * Web
  */
 $coreConfig->saveConfig (Mage_Core_Model_Cookie::XML_PATH_COOKIE_LIFETIME, '86400');
+$coreConfig->saveConfig (Mage_Core_Model_Cookie::XML_PATH_COOKIE_PATH, '/');
+
+$coreConfig->saveconfig (Mage_Core_Model_Session_Abstract::XML_PATH_USE_FRONTEND_SID, '0');
+$coreConfig->saveConfig (Mage_Admin_Model_Session::XML_PATH_ALLOW_SID_FOR_ADMIN_AREA, '1');
 
 /**
  * Design
