6e1254ea641cd7af362584d85927fe3c8e3334e6
Eneias Ramos de Melo <eneias@gamuza.com.br>
2022-08-14 12:45:14 -0300

    Adicionado modulo Gamuza_PagseguroPro

---
+++ magento-lts-1.9.4.x/app/code/local/Gamuza/PagSeguroPro/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package     Gamuza_PagSeguroPro
+ * @copyright   Copyright (c) 2022 Gamuza Technologies (http://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+class Gamuza_PagSeguroPro_Helper_Data extends RicardoMartins_PagSeguroPro_Helper_Data
+{
+}
+
