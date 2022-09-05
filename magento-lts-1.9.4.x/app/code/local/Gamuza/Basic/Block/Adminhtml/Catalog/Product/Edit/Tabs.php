e4c3c3017f5debe255a008d2a40dd11145d03979
Eneias Ramos de Melo <eneias@gamuza.com.br>
2022-09-05 19:11:40 -0300

    Atualizado modulo Gamuza_Basic: comentado rotina que remove tabs related, upsell, crosssell no cadastro do produto via admin

--- magento-lts-1.9.4.x/app/code/local/Gamuza/Basic/Block/Adminhtml/Catalog/Product/Edit/Tabs.php
+++ magento-lts-1.9.4.x/app/code/local/Gamuza/Basic/Block/Adminhtml/Catalog/Product/Edit/Tabs.php
@@ -11,10 +11,10 @@
 class Gamuza_Basic_Block_Adminhtml_Catalog_Product_Edit_Tabs
     extends Mage_Adminhtml_Block_Catalog_Product_Edit_Tabs
 {
-    const CATALOG_PRODUCT_GROUP_ID_9  = 'group_9';
-    const CATALOG_PRODUCT_GROUP_ID_11 = 'group_11';
-    const CATALOG_PRODUCT_GROUP_ID_12 = 'group_12';
-    const CATALOG_PRODUCT_GROUP_ID_17 = 'group_17';
+    const CATALOG_PRODUCT_GROUP_ID_9  = 'group_9';  // meta_information
+    const CATALOG_PRODUCT_GROUP_ID_11 = 'group_11'; // recurring_profile
+    const CATALOG_PRODUCT_GROUP_ID_12 = 'group_12'; // custom_design
+    const CATALOG_PRODUCT_GROUP_ID_17 = 'group_17'; // gift_message
 
     protected function _prepareLayout ()
     {
@@ -25,9 +25,11 @@ class Gamuza_Basic_Block_Adminhtml_Catalog_Product_Edit_Tabs
         $this->removeTab (self::CATALOG_PRODUCT_GROUP_ID_12);
         $this->removeTab (self::CATALOG_PRODUCT_GROUP_ID_17);
 
+        /*
         $this->removeTab ('related');
         $this->removeTab ('upsell');
         $this->removeTab ('crosssell');
+        */
 
         return $result;
     }
