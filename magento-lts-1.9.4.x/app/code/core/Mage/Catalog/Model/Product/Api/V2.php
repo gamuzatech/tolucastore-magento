d81402a8e2f7f2826078e4e876f57ad9e950ebcb
Eneias Ramos de Melo <eneias@gamuza.com.br>
2022-12-04 18:04:40 -0300

    Atualizado arquivos do core: github -> OpenMage-magento-lts-1.9.4.x-master.zip

--- magento-lts-1.9.4.x/app/code/core/Mage/Catalog/Model/Product/Api/V2.php
+++ magento-lts-1.9.4.x/app/code/core/Mage/Catalog/Model/Product/Api/V2.php
@@ -204,7 +204,7 @@ class Mage_Catalog_Model_Product_Api_V2 extends Mage_Catalog_Model_Product_Api
      * @param array      $productData
      * @param string|int $store
      * @param string     $identifierType
-     * @return bool
+     * @return true|void
      */
     public function multiUpdate($productIds, $productData, $store = null, $identifierType = null)
     {
@@ -225,11 +225,9 @@ class Mage_Catalog_Model_Product_Api_V2 extends Mage_Catalog_Model_Product_Api
 
         if (empty($failMessages)) {
             return true;
-        } else {
-            $this->_fault('partially_updated', implode("\n", $failMessages));
         }
 
-        return false;
+        $this->_fault('partially_updated', implode("\n", $failMessages));
     }
 
     /**
@@ -313,9 +311,9 @@ class Mage_Catalog_Model_Product_Api_V2 extends Mage_Catalog_Model_Product_Api
         }
 
         if (property_exists($productData, 'tier_price')) {
-             $tierPrices = Mage::getModel('catalog/product_attribute_tierprice_api_V2')
+            $tierPrices = Mage::getModel('catalog/product_attribute_tierprice_api_V2')
                  ->prepareTierPrices($product, $productData->tier_price);
-             $product->setData(Mage_Catalog_Model_Product_Attribute_Tierprice_Api_V2::ATTRIBUTE_CODE, $tierPrices);
+            $product->setData(Mage_Catalog_Model_Product_Attribute_Tierprice_Api_V2::ATTRIBUTE_CODE, $tierPrices);
         }
     }
 
