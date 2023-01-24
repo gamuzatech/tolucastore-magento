4c9cf8511cfa7b2bcaef873d6092090c75632137
Eneias Ramos de Melo <eneias@gamuza.com.br>
2023-01-23 22:17:09 -0300

    Atualizado modulo Gamuza_Brazil: para adicionar as coisas que só existem por aqui ;)

---
+++ magento-lts-1.9.4.x/app/code/local/Gamuza/Brazil/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package     Gamuza_Brazil
+ * @copyright   Copyright (c) 2023 Gamuza Technologies (http://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+class Gamuza_Brazil_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    const CUSTOMER_ATTRIBUTE_RGIE = 'br_rgie';
+}
+
---
+++ magento-lts-1.9.4.x/app/code/local/Gamuza/Brazil/sql/brazil_setup/mysql4-install-0.0.1.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * @package     Gamuza_Brazil
+ * @copyright   Copyright (c) 2023 Gamuza Technologies (http://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+$installer = new Mage_Customer_Model_Entity_Setup('brazil_setup');
+$installer->startSetup ();
+
+$installer->addAttribute(
+    'customer',
+    Gamuza_Brazil_Helper_Data::CUSTOMER_ATTRIBUTE_RGIE,
+    array(
+        'type'         => 'varchar',
+        'length'       => 255,
+        'input'        => 'text',
+        'label'        => Mage::helper ('brazil')->__('RG/IE'),
+        'visible'      => true,
+        'required'     => false,
+        'user_defined' => false,
+        'unique'       => false,
+    )
+);
+
+$forms = array(
+    'adminhtml_customer',
+    'adminhtml_checkout',
+    'customer_account_create',
+    'customer_account_edit',
+    'checkout_register',
+);
+
+$attribute = Mage::getSingleton ('eav/config')->getAttribute(
+    $installer->getEntityTypeId ('customer'), Gamuza_Brazil_Helper_Data::CUSTOMER_ATTRIBUTE_RGIE)
+;
+$attribute->setData ('used_in_forms', $forms)
+    ->setData('is_system', true)
+    ->setData('sort_order', 101)
+;
+$attribute->save ();
+
+$installer->endSetup ();
+
