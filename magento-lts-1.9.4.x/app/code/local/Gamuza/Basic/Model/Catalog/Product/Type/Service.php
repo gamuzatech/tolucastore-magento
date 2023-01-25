a2abd6974dbe44781e7c6955cff6bdb071faf0a6
Eneias Ramos de Melo <eneias@gamuza.com.br>
2023-01-25 20:38:53 -0300

    Atualizado modulo Gamuza_Basic: adicionado tipo de produto (type_id) para serviço (service)

---
+++ magento-lts-1.9.4.x/app/code/local/Gamuza/Basic/Model/Catalog/Product/Type/Service.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package     Gamuza_Basic
+ * @copyright   Copyright (c) 2023 Gamuza Technologies (http://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+/**
+ * Service product type implementation
+ */
+class Gamuza_Basic_Model_Catalog_Product_Type_Service
+    extends Mage_Catalog_Model_Product_Type_Virtual
+{
+}
+
