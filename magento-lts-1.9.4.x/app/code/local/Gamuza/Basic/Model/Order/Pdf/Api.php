44adcc85f48dc2c70a8a7de6b32f13819a4d97db
Eneias Ramos de Melo <eneias@gamuza.com.br>
2023-02-12 00:05:20 -0300

    Atualizado modulo Gamuza_Basic: adicionado paths basic_order_pdf.{invoice,shipment,creditmemo}

--- magento-lts-1.9.4.x/app/code/local/Gamuza/Basic/Model/Order/Pdf/Api.php
+++ magento-lts-1.9.4.x/app/code/local/Gamuza/Basic/Model/Order/Pdf/Api.php
@@ -10,6 +10,111 @@
  */
 class Gamuza_Basic_Model_Order_Pdf_Api extends Mage_Api_Model_Resource_Abstract
 {
+    public function invoice ($incrementId, $protectCode)
+    {
+        $order = $this->_getOrder ($incrementId, $protectCode);
+
+        $collection = Mage::getModel ('sales/order_invoice')->getCollection ()
+            ->addFieldToFilter ('order_id', array ('eq' => $order->getId ()))
+        ;
+
+        if (!$collection->getSize ())
+        {
+            $this->_fault ('invoice_not_exists');
+        }
+
+        $emulation = Mage::getModel ('core/app_emulation');
+
+        $oldEnvironment = $emulation->startEnvironmentEmulation(
+            Mage_Core_Model_App::ADMIN_STORE_ID,
+            Mage_Core_Model_App_Area::AREA_ADMINHTML,
+            true
+        );
+
+        $result = array ();
+
+        foreach ($collection as $invoice)
+        {
+            $result [] = $invoice;
+        }
+
+        $pdf = Mage::getModel ('sales/order_pdf_invoice')->getPdf ($result);
+
+        $emulation->stopEnvironmentEmulation($oldEnvironment);
+
+        return base64_encode ($pdf->render ());
+    }
+
+    public function shipment ($incrementId, $protectCode)
+    {
+        $order = $this->_getOrder ($incrementId, $protectCode);
+
+        $collection = Mage::getModel ('sales/order_shipment')->getCollection ()
+            ->addFieldToFilter ('order_id', array ('eq' => $order->getId ()))
+        ;
+
+        if (!$collection->getSize ())
+        {
+            $this->_fault ('shipment_not_exists');
+        }
+
+        $emulation = Mage::getModel ('core/app_emulation');
+
+        $oldEnvironment = $emulation->startEnvironmentEmulation(
+            Mage_Core_Model_App::ADMIN_STORE_ID,
+            Mage_Core_Model_App_Area::AREA_ADMINHTML,
+            true
+        );
+
+        $result = array ();
+
+        foreach ($collection as $shipment)
+        {
+            $result [] = $shipment;
+        }
+
+        $pdf = Mage::getModel ('sales/order_pdf_shipment')->getPdf ($result);
+
+        $emulation->stopEnvironmentEmulation($oldEnvironment);
+
+        return base64_encode ($pdf->render ());
+    }
+
+    public function creditmemo ($incrementId, $protectCode)
+    {
+        $order = $this->_getOrder ($incrementId, $protectCode);
+
+        $collection = Mage::getModel ('sales/order_creditmemo')->getCollection ()
+            ->addFieldToFilter ('order_id', array ('eq' => $order->getId ()))
+        ;
+
+        if (!$collection->getSize ())
+        {
+            $this->_fault ('creditmemo_not_exists');
+        }
+
+        $emulation = Mage::getModel ('core/app_emulation');
+
+        $oldEnvironment = $emulation->startEnvironmentEmulation(
+            Mage_Core_Model_App::ADMIN_STORE_ID,
+            Mage_Core_Model_App_Area::AREA_ADMINHTML,
+            true
+        );
+
+        $result = array ();
+
+        foreach ($collection as $creditmemo)
+        {
+            $result [] = $creditmemo;
+        }
+
+        $pdf = Mage::getModel ('sales/order_pdf_creditmemo')->getPdf ($result);
+
+        $emulation->stopEnvironmentEmulation($oldEnvironment);
+
+        return base64_encode ($pdf->render ());
+    }
+
     public function service ($incrementId, $protectCode)
     {
         $order = $this->_getOrder ($incrementId, $protectCode);
