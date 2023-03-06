d52ca3cb6f638323d84c83797d98eb12eebd97e9
Eneias Ramos de Melo <eneias@gamuza.com.br>
2023-03-06 19:35:23 -0300

    Atualizado modulo Gamuza_Basic: removido sufixo MOBILE

--- magento-lts-1.9.4.x/app/code/local/Gamuza/Basic/Helper/Data.php
+++ magento-lts-1.9.4.x/app/code/local/Gamuza/Basic/Helper/Data.php
@@ -48,11 +48,9 @@ class Gamuza_Basic_Helper_Data extends Mage_Core_Helper_Abstract
     const ORDER_SUFFIX_APP    = 'APP';
     const ORDER_SUFFIX_BOT    = 'BOT';
     const ORDER_SUFFIX_PDV    = 'PDV';
-    const ORDER_SUFFIX_SAT    = 'SAT';
     const ORDER_SUFFIX_ADMIN  = 'ADMIN';
     const ORDER_SUFFIX_STORE  = 'STORE';
     const ORDER_SUFFIX_OTHER  = 'OTHER';
-    const ORDER_SUFFIX_MOBILE = 'MOBILE';
 
     const PRODUCT_ATTRIBUTE_BRAND = 'brand';
     const PRODUCT_ATTRIBUTE_CODE = 'code';
--- magento-lts-1.9.4.x/app/code/local/Gamuza/Basic/Model/Eav/Entity/Type.php
+++ magento-lts-1.9.4.x/app/code/local/Gamuza/Basic/Model/Eav/Entity/Type.php
@@ -36,37 +36,27 @@ class Gamuza_Basic_Model_Eav_Entity_Type extends Mage_Eav_Model_Entity_Type
         {
             $suffix = Gamuza_Basic_Helper_Data::ORDER_SUFFIX_ADMIN;
         }
-        else if ($storeId == Mage_Core_Model_App::DISTRO_STORE_ID)
+
+        if ($storeId == Mage_Core_Model_App::DISTRO_STORE_ID)
         {
             $suffix = Gamuza_Basic_Helper_Data::ORDER_SUFFIX_STORE;
         }
 
-        $isMobile = Mage::helper ('basic')->isMobile ();
-
         $isApp = $quote->getData (Gamuza_Basic_Helper_Data::ORDER_ATTRIBUTE_IS_APP);
         $isBot = $quote->getData (Gamuza_Basic_Helper_Data::ORDER_ATTRIBUTE_IS_BOT);
-/*
-        $isBot = Mage::helper ('core')->isModuleEnabled ('Toluca_Bot')
-            && (!strcmp (Mage::app ()->getRequest ()->getControllerModule (), 'Toluca_Bot')
-            xor (!strcmp (Mage::app ()->getRequest ()->getControllerModule (), 'Gamuza_JsonApi')
-            && strpos (Mage::app ()->getRequest ()->getRawBody (), self::API_METHOD_BOT_CHAT_MESSAGE) !== false))
-        ;
-*/
         $isPdv = $quote->getData (Gamuza_Basic_Helper_Data::ORDER_ATTRIBUTE_IS_PDV);
 
-        if ($isMobile)
-        {
-            $suffix = Gamuza_Basic_Helper_Data::ORDER_SUFFIX_MOBILE;
-        }
-        else if ($isApp)
+        if ($isApp)
         {
             $suffix = Gamuza_Basic_Helper_Data::ORDER_SUFFIX_APP;
         }
-        else if ($isBot)
+
+        if ($isBot)
         {
             $suffix = Gamuza_Basic_Helper_Data::ORDER_SUFFIX_BOT;
         }
-        else if ($isPdv)
+
+        if ($isPdv)
         {
             $suffix = Gamuza_Basic_Helper_Data::ORDER_SUFFIX_PDV;
         }
