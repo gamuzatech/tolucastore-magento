08b2f5fb65695235074c8859862260dfa8bcaa51
Eneias Ramos de Melo <eneias@gamuza.com.br>
2023-04-19 11:55:56 -0300

    Atualizado modulo Toluca_PDV: adicionado informações de pagamento, frete e produtos no rascunho do caixa

--- magento-lts-1.9.4.x/app/code/local/Toluca/PDV/Block/Adminhtml/Cashier/Draft.php
+++ magento-lts-1.9.4.x/app/code/local/Toluca/PDV/Block/Adminhtml/Cashier/Draft.php
@@ -7,18 +7,11 @@
 
 class Toluca_PDV_Block_Adminhtml_Cashier_Draft extends Mage_Adminhtml_Block_Template
 {
-    private $_wordwrap = 48; // default
-
     public function __construct ()
     {
         parent::__construct ();
 
         $this->_wordwrap = intval (Mage::getStoreConfig ('sales/order_draft/word_wrap'));
-    }
-
-    public function getWordWrap ()
-    {
-        return $this->_wordwrap;
     }
 
     public function getLineSeparator ($title = null)
@@ -40,6 +33,83 @@ class Toluca_PDV_Block_Adminhtml_Cashier_Draft extends Mage_Adminhtml_Block_Temp
         }
 
         return $result;
+    }
+
+    public function getOrderPayments ($cashier, $operator, $history)
+    {
+        $collection = $this->_getOrderCollection ($cashier, $operator, $history);
+
+        $collection->getSelect ()
+            ->join(
+                array ('sfop' => Mage::getSingleton ('core/resource')->getTablename ('sales/order_payment')),
+                'main_table.entity_id = sfop.parent_id',
+                array(
+                    'payment_method' => 'sfop.method',
+                    'payment_count'  => 'COUNT(sfop.entity_id)',
+                    'payment_amount' => 'SUM(sfop.base_amount_paid)',
+                )
+            )
+            ->group ('sfop.method')
+            ->order ('payment_count DESC')
+            ->order ('payment_amount DESC')
+        ;
+
+        return $collection;
+    }
+
+    public function getOrderShipments ($cashier, $operator, $history)
+    {
+        $collection = $this->_getOrderCollection ($cashier, $operator, $history);
+
+        $collection->getSelect ()
+            ->columns (array(
+                'shipment_count'  => 'COUNT(entity_id)',
+                'shipment_amount' => 'SUM(base_shipping_amount)',
+            ))
+            ->group ('shipping_method')
+            ->order ('shipment_count DESC')
+            ->order ('shipment_amount DESC')
+        ;
+
+        return $collection;
+    }
+
+    public function getOrderProducts ($cashier, $operator, $history)
+    {
+        $collection = $this->_getOrderCollection ($cashier, $operator, $history);
+
+        $collection = Mage::getModel ('sales/order_item')->getCollection ()
+            ->addFieldToFilter ('order_id', array ('in' => $collection->getAllIds ()))
+            ->addFieldToFilter ('parent_item_id', array ('null' => true))
+        ;
+
+        $collection->getSelect ()
+            ->columns (array(
+                'product_count'  => 'SUM(qty_invoiced)',
+                'product_amount' => 'SUM(base_row_total)', // BUG: base_row_invoiced is NULL
+            ))
+            ->group ('sku')
+            ->order ('product_count DESC')
+            ->order ('product_amount DESC')
+        ;
+
+        return $collection;
+    }
+
+    private function _getOrderCollection ($cashier, $operator, $history)
+    {
+        $collection = Mage::getModel ('sales/order')->getCollection ()
+            ->addFieldToFilter ('state', array ('in' => array (
+                Mage_Sales_Model_Order::STATE_PROCESSING,
+                Mage_Sales_Model_Order::STATE_COMPLETE,
+            )))
+            ->addFieldToFilter ('is_pdv', array ('eq' => true))
+            ->addFieldToFilter ('pdv_cashier_id', array ('eq' => $cashier->getId ()))
+            ->addFieldToFilter ('pdv_operator_id', array ('eq' => $operator->getId ()))
+            ->addFieldToFilter ('pdv_history_id', array ('eq' => $history->getId ()))
+        ;
+
+        return $collection;
     }
 }
 
