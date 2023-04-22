2eb5ae681c3c0b260b7db0b04b618dc4d1053a68
Eneias Ramos de Melo <eneias@gamuza.com.br>
2023-04-21 22:20:44 -0300

    Atualizado modulo Toluca_PDV: adicionado filtro para exibir ou não pedidos pendentes no valor total de pedidos do caixa

--- magento-lts-1.9.4.x/app/code/local/Toluca/PDV/Model/Cashier/Api.php
+++ magento-lts-1.9.4.x/app/code/local/Toluca/PDV/Model/Cashier/Api.php
@@ -495,15 +495,19 @@ class Toluca_PDV_Model_Cashier_Api extends Mage_Api_Model_Resource_Abstract
     public function _getOrderCollection ($cashier, $operator, $history)
     {
         $collection = Mage::getModel ('sales/order')->getCollection ()
-            ->addFieldToFilter ('state', array ('in' => array (
-                Mage_Sales_Model_Order::STATE_PROCESSING,
-                Mage_Sales_Model_Order::STATE_COMPLETE,
-            )))
             ->addFieldToFilter ('is_pdv', array ('eq' => true))
             ->addFieldToFilter ('pdv_cashier_id', array ('eq' => $cashier->getId ()))
             ->addFieldToFilter ('pdv_operator_id', array ('eq' => $operator->getId ()))
             ->addFieldToFilter ('pdv_history_id', array ('eq' => $history->getId ()))
         ;
+
+        if (!Mage::getStoreConfigFlag ('pdv/cashier/show_pending_orders'))
+        {
+             $collection->addFieldToFilter ('state', array ('in' => array (
+                 Mage_Sales_Model_Order::STATE_PROCESSING,
+                 Mage_Sales_Model_Order::STATE_COMPLETE,
+             )));
+        }
 
         return $collection;
     }
