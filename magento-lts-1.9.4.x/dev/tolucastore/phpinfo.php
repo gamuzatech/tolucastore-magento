507da18fcf638a22419375beb0f69e399421b5c3
Eneias Ramos de Melo <eneias@gamuza.com.br>
2023-04-25 12:51:46 -0300

    Adicionado script dev/tolucastore/phpinfo.php

---
+++ magento-lts-1.9.4.x/dev/tolucastore/phpinfo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package     Gamuza_Basic
+ * @copyright   Copyright (c) 2023 Gamuza Technologies (https://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+phpinfo();
+
