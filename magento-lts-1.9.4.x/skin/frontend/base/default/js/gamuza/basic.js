83a5dbd770f1349e0e64398c937bbf09aad61e1f
Eneias Ramos de Melo <eneias@gamuza.com.br>
2023-05-06 02:55:08 -0300

    Atualizado modulo Gamuza_Basic: adicionado css e js para bootstrap

---
+++ magento-lts-1.9.4.x/skin/frontend/base/default/js/gamuza/basic.js
@@ -0,0 +1,18 @@
+/**
+ * @package     Gamuza_Basic
+ * @copyright   Copyright (c) 2023 Gamuza Technologies. (https://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+/**
+ * apply Bootstrap stuff
+ */
+$j(document).ready(function(){
+    $j('a.button, button.button').removeClass('button').addClass('btn btn-primary');
+    $j('a.button, button.btn-checkout').removeClass('btn-checkout').addClass('btn-lg');
+    $j('input.input-text').addClass('form-control');
+    $j('input.checkbox:checkbox').addClass('form-check-input');
+    $j('input.radio:radio').addClass('form-check-input');
+    $j('select').addClass('form-select');
+});
+
