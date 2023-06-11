0beae2e17bd5aa3b66f6f8598ad916ecf91b2b97
Eneias Ramos de Melo <eneias@gamuza.com.br>
2023-06-11 13:09:13 -0300

    Adicionado modulo Gamuza_SMTPPro

---
+++ magento-lts-1.9.4.x/app/code/local/Gamuza/SMTPPro/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * @package     Gamuza_SMTPPro
+ * @copyright   Copyright (c) 2023 Gamuza Technologies (https://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+class Gamuza_SMTPPro_Helper_Data extends Aschroder_SMTPPro_Helper_Data
+{
+}
+
